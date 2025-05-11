cd0e5a7ea64aa93886c2253659c316aedb6d8393
Developer <99184367+durgeshmalviya@users.noreply.github.com>
2025-05-10 22:44:06 -0700

    Update page1.tsx

--- src/app/blog/page1.tsx
+++ src/app/blog/page1.tsx
@@ -1,10 +1,10 @@
 "use client";
 import { Spotlight } from "@/components/ui/spotlight-new"
 import { motion } from "framer-motion";
-import { FloatingNavDemo } from "./nav";
+ 
 export default function SpotlightNewDemo() {
   return (<>
-  <FloatingNavDemo/>
+  
     <div className="h-screen w-full rounded-md flex md:items-center md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden">
       <Spotlight />
       <div className=" p-4 max-w-7xl  mx-auto relative z-10  w-full pt-20 md:pt-0">
