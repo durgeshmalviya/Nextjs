8524c8b1270e494ac1c6bf30c308dc16d2bb461f
Developer <99184367+durgeshmalviya@users.noreply.github.com>
2025-05-10 23:08:34 -0700

    Update profile.tsx

--- src/components/ui/profile.tsx
+++ src/components/ui/profile.tsx
@@ -1,8 +1,6 @@
-// pages/profile.tsx
-import { withPageAuthRequired } from '@auth0/nextjs-auth0';
+ 
 
 export default function Profile({ user }: any) {
   return <pre>{JSON.stringify(user, null, 2)}</pre>;
 }
-
-export const getServerSideProps = withPageAuthRequired();
+ 
